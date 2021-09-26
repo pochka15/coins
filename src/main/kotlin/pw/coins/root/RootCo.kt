df27cab2c141b21c1eb96809431c5efdf23a74f6
Artem Kashkou <koshkov-artem@mail.ru>
2021-09-26 23:00:57 +0200

    Add a root redirect

---
+++ src/main/kotlin/pw/coins/root/RootCo.kt
@@ -0,0 +1,16 @@
+package pw.coins.root
+
+import org.springframework.ui.ModelMap
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
+
+@RestController
+@RequestMapping("/")
+class RootCo {
+    @GetMapping
+    fun redirectSwagger(model: ModelMap): ModelAndView {
+        return ModelAndView("redirect:/swagger-ui/", model)
+    }
+}
