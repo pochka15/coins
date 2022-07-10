8a5c4f075798fd4541beaacbc3a0e824a3af6591
Artsiom Kashkou <artsiom.kashkou@jetbrains.com>
2022-07-10 14:46:14 +0200

    Add usos config vars

--- backend/src/main/kotlin/pw/coins/security/oauth/UsosConfiguration.kt
+++ backend/src/main/kotlin/pw/coins/security/oauth/UsosConfiguration.kt
@@ -2,17 +2,21 @@ package pw.coins.security.oauth
 
 import com.github.scribejava.core.oauth.OAuth10aService
 import com.github.scribejava.httpclient.okhttp.OkHttpHttpClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class UsosConfiguration {
+class UsosConfiguration(
+    @Value("\${usos.apiKey}") val apiKey: String,
+    @Value("\${usos.apiSecret}") val apiSecret: String,
+) {
     @Bean
     fun oauthService(): OAuth10aService {
         return UsosAuthService(
             api = UsosApi(),
-            apiKey = "wdJbgtUSwTrRf5S328eD",
-            apiSecret = "PqAr2rfUqXpm6tV6xtpAC3g3zUrPUfGuH8eMANZJ",
+            apiKey = apiKey,
+            apiSecret = apiSecret,
             callback = "http://localhost:3000/oauth/usos-callback",
             scope = "cards",
             debugStream = System.out,
